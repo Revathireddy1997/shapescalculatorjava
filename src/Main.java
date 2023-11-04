726acd12204b20261cf42dafec2cd42732fc6e8f
revathi <revathireddy97@gmail.com>
2023-11-04 12:28:49 -0400

    calculating area and perimeter for circle,square and rectangle

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+class Circle {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+class Square {
+    private double side;
+    public Square(double side) {
+        this.side = side;
+    }
+    public double getArea() {
+        return side * side;
+    }
+    public double getPerimeter() {
+        return 4 * side;
+    }
+}
+class Rectangle {
+    private double length;
+    private double width;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Square square = new Square(4.0);
+        Rectangle rectangle = new Rectangle(6.0, 8.0);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+
+        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+    }
+}
